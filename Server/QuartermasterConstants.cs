011518a10747b2e7686f1d3b210124342ab3e01e
ShaneeexD <31047364+ShaneeexD@users.noreply.github.com>
2026-07-16 23:39:44 +0100

    Bump version to 1.0.7

--- Server/QuartermasterConstants.cs
+++ Server/QuartermasterConstants.cs
@@ -56,8 +56,8 @@ public static class QuartermasterConstants
 
     public static class Versions
     {
-        public static readonly Version Current = new Version("1.0.6");
-        public const string CurrentString = "1.0.6";
+        public static readonly Version Current = new Version("1.0.7");
+        public const string CurrentString = "1.0.7";
     }
 
     public static class FirestoreCollections
